5618eee883b7d3ec253fb2128fa028fecd682f84
Gina W <watanabegina26@gmail.com>
2017-05-25 15:54:42 -1000

    Added direct message function when a user follows.

--- bot.js
+++ bot.js
@@ -6,3 +6,23 @@ let config = require('./config');
 
 console.log(config);
 console.log();
+
+let T = new Twit(config);
+
+let stream = T.stream('user');
+stream.on('follow',followed);
+
+function followed(eventMessage) {
+  console.log("Someone has followed us!");
+  let name = eventMessage.source.name;
+  let screenName = eventMessage.source.screen_name;
+  T.post('direct_messages/new', {screen_name: screenName,
+  text: 'Aloha ' + name + '! Thanks for your interest in Jr. DevLeague! For more info, check out http://www.jrdevleague.com/.'},
+  function(err, data, response){
+    if(err){
+      console.log(err);
+    }else{
+      console.log('The message has been sent!');
+    }
+  });
+}
